dd7e2c7fe936387f2c6df97769d6b062dfc86649
Tyythedeveloper33 <webstertyler81@yahoo.com>
2025-04-04 18:30:52 -0400

    fixed white overlay, footer issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,12 +20,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`h-full flex flex-col ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-         <Navigation />
-        <main>{children}</main>
+        <Navigation />
+
+        <main className="flex-1">{children}</main>
+
         <Footer />
       </body>
     </html>
