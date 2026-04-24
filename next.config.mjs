f07983a08facd52998a95ff9baa174f563102194
prepace <pace@flyingovertime.org>
2026-04-23 21:10:42 -0500

    next-version

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['your-image-domain.com'], // Add external image domains here if needed
+      remotePatterns: [],
     },
   };
 
